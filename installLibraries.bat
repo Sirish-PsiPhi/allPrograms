66d7e90a4ea598ef6badf3a3c55bf4703c10fa91
Sirish <b17sirish@gmail.com>
2022-03-23 16:48:24 +0530

    updated package

--- installLibraries.bat
+++ installLibraries.bat
@@ -5,7 +5,7 @@
 cls
 
 pip install heuristicsearch==0.1.1
-pip install conceptlearning==0.0.5
+pip install conceptlearning==0.0.8
 pip install decision-tree-ID3-Algorithm==0.0.5
 pip install backpropagation==0.0.3
 pip install scikit-learn
